db4f54cf2befea3f920533da150f9749b5f8bdce
aishuraja <aishuraja2001@gmail.com>
2024-11-03 13:02:33 -0500

    commit

--- dataStructures/Heap.py
+++ dataStructures/Heap.py
@@ -39,4 +39,34 @@ def heapsort(nums):
     return newList
 heapsort([1,3,10,-1,8,5,4])
 
+# Heap pushpop 
+# tIME: O(log n)
+heapq.heappushpop(arr, 99)
+print(arr)
 
+# MAX HEAP 
+B = [-3, 2, 10, 7, 9, 6, -2, 5]
+for i in range(len(B)):
+    B[i] = -B[i]  
+heapq.heapify(B)
+
+print(B)   # it has desc to asec and has negative value
+
+largest = -heapq.heappop(B) # now it pops the correct max value out of heap 
+print(largest)
+
+heapq.heappush(B, -4) #Inserts 4
+print(B)
+
+# Putting tuples of items on the heap 
+
+C = [1,1,2,2,2,3,3,3,3,4]
+from collections import Counter  # gives key:value pairs 
+counter = Counter(C)
+print(counter)
+
+heap = []
+for k, v in counter.items():
+    heapq.heappush(heap, (v,k))   # here it takes value 1st (frequency of key) then key (v,k) , if encounters tie with same values then compares with key (k, v)
+
+print(heap) # smallest (min) frequency shows on the top. 
